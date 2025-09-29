49ed58576ff4a0d424bced68689623d88b79b568
pjsmacbook <princessgvidad@gmail.com>
2025-09-29 22:45:53 +0800

    created multiple activities

--- app/src/main/java/com/example/co2_/WelcomeActivity.kt
+++ app/src/main/java/com/example/co2_/WelcomeActivity.kt
@@ -1,10 +1,22 @@
 package com.example.co2_
+
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.welcome)
+
+        // find the button by its ID (make sure it's the same in welcome.xml)
+        val btnGetStarted = findViewById<Button>(R.id.bookTitle0)
+
+        // set click listener
+        btnGetStarted.setOnClickListener {
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
