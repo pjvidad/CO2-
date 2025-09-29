aecae7a23e3c987576e4a7b330f636bbd946a079
pjsmacbook <princessgvidad@gmail.com>
2025-09-29 16:38:01 +0800

    removed github repository for barchart

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,7 +45,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation(libs.mpandroidchart)
 
     // Add these new ones for more UI components:
     implementation("androidx.viewpager2:viewpager2:1.0.0")
