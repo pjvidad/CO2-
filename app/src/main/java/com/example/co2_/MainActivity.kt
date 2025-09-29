c312061c23a4f7608b476dec42d8478c9eb9e4fe
pjsmacbook <princessgvidad@gmail.com>
2025-09-29 15:20:31 +0800

    added fragments

--- app/src/main/java/com/example/co2_/MainActivity.kt
+++ app/src/main/java/com/example/co2_/MainActivity.kt
@@ -5,7 +5,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
+import android.widget.ImageButton
+import androidx.fragment.app.Fragment
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,29 +14,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // Default fragment
-        //replaceFragment(HomeFragment())
+        replaceFragment(HomeFragment())
 
         // Nav button listeners
-        //findViewById<ImageButton>(R.id.navHome).setOnClickListener {
-          //  replaceFragment(HomeFragment())
-        //}
+        findViewById<ImageButton>(R.id.navHome).setOnClickListener {
+            replaceFragment(HomeFragment())
+        }
 
-        //findViewById<ImageButton>(R.id.navBook).setOnClickListener {
-          //  replaceFragment(BookFragment())
-        //}
+        findViewById<ImageButton>(R.id.navBook).setOnClickListener {
+            replaceFragment(BookFragment())
+        }
 
-        //findViewById<ImageButton>(R.id.navCalc).setOnClickListener {
-         //   replaceFragment(CalcFragment())
-        //}
+        findViewById<ImageButton>(R.id.navCalc).setOnClickListener {
+            replaceFragment(TrackerFragmentFragment())
+        }
 
-        //findViewById<ImageButton>(R.id.navBag).setOnClickListener {
-         //   replaceFragment(BagFragment())
-        //}
+        findViewById<ImageButton>(R.id.navBag).setOnClickListener {
+            replaceFragment(ShopFragment())
+        }
     }
 
-   // private fun replaceFragment(fragment: Fragment) {
-     //   supportFragmentManager.beginTransaction()
-         //   .replace(R.id.fragmentContainer, fragment)
-       //     .commit()
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, fragment)
+            .commit()
     }
-//}
+}
+
