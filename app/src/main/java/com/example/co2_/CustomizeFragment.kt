3616502eae87397bb8bc6f862d7cca91c4e6219f
pjsmacbook <princessgvidad@gmail.com>
2025-10-21 19:29:49 +0800

    shop navigation

--- app/src/main/java/com/example/co2_/CustomizeFragment.kt
+++ app/src/main/java/com/example/co2_/CustomizeFragment.kt
@@ -1,28 +1,31 @@
 package com.example.co2_
+
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import android.widget.Button
+import androidx.fragment.app.Fragment
 
-class ShopFragment : Fragment() {
+class CustomizeFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.shop_redeem, container, false)
+        return inflater.inflate(R.layout.shop_customize, container, false)
     }
+
+    @SuppressLint("CommitTransaction", "ResourceType")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val customizeButton = view.findViewById<Button>(R.id.button_customize)
+        val redeemButton = view.findViewById<Button>(R.id.button_redeem)
 
-        customizeButton.setOnClickListener {
+        redeemButton.setOnClickListener {
             parentFragmentManager.beginTransaction()
-                .replace(R.id.fragmentContainer, CustomizeFragment()) // Replace with your main container ID
-                .addToBackStack(null)
-                .commit()
+                .replace(R.id.fragmentContainer, ShopFragment())
         }
     }
 }
